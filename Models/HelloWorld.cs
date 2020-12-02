8dfc6ad6322c5071a2684ff309f26cdfe057e29b
ericbanker12 <ericbanker12@gmail.com>
2020-12-01 19:37:48 -0600

    Create basic ASP.NET + Blazor, static webapp

---
+++ Models/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_Hello_World.Models
+{
+    public class HelloWorld
+    {
+        public string language { get; set; }
+
+        public string image { get; set; }
+
+        public string text { get; set; }
+    }
+}
